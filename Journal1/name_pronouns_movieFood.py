fe0fe7b01688af4ec3d21e7da4273930fa9f2fd7
InvinciblePyro <samrexspivey@gmail.com>
2026-03-31 16:31:24 -0700

    finished name n pronouns n stuff

--- Journal1/name_pronouns_movieFood.py
+++ Journal1/name_pronouns_movieFood.py
@@ -1,3 +1,5 @@
 name = "Sameul Rex Spivey"
 pronouns = "he/him"
-fav_movie_and_food = "My favorite food is either Sushi or Burgers (yes i'm American how'd you know). "
+fav_movie_and_food = "My favorite food is either Sushi or Burgers (yes i'm American how'd you know).\nOne of my favorite movies ever, I have a lot, is seven samurai."
+
+print(f"{name}\n{pronouns}\n{fav_movie_and_food}")
