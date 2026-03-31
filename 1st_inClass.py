da7aabf81374d64a9578f371b9b6950d6eb66dac
InvinciblePyro <samrexspivey@gmail.com>
2026-03-31 14:04:32 -0700

    shortened 1st_inClass

--- 1st_inClass.py
+++ 1st_inClass.py
@@ -2,25 +2,21 @@ import random
 import string
 import time
 
-txt = "HELLO"
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits): 
     return ''.join(random.choice(chars) for _ in range(size))
 
-def funTextThing(text, speed):
+def TxtPerLetter(text, speed):
     rand_word = (id_generator(len(text)))
-    new_rand_word = rand_word
-
     for i in range(len(text)):
-        #rand_word = (id_generator(1)) + rand_word[i+1:]
-        while new_rand_word[i] != text[i]: 
+        while rand_word[i] != text[i]: 
             time.sleep(speed)
-            print(new_rand_word)
-            new_rand_word = (new_rand_word[0:i]) + (id_generator(1)) + (rand_word[i+1:])
-        print(new_rand_word + "🟢") 
+            print(rand_word)
+            rand_word = (rand_word[0:i]) + (id_generator(1)) + (rand_word[i+1:])
+        print(rand_word + "🟢") 
 
         
 def main():
-    funTextThing(txt, 0.03)
+    TxtPerLetter("HELLOWORLD", 0.03)
 
 
 
