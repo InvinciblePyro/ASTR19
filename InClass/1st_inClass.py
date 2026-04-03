e4eaea972367a4e7ff2a535a86b68631fd636926
InvinciblePyro <samrexspivey@gmail.com>
2026-04-02 20:59:04 -0700

    ignore this: trying to make another cool text thing

--- InClass/1st_inClass.py
+++ InClass/1st_inClass.py
@@ -5,7 +5,7 @@ import time
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits): 
     return ''.join(random.choice(chars) for _ in range(size))
 
-def TxtPerLetter(text, speed):
+def TxtLeftToRight(text, speed):
     rand_word = (id_generator(len(text)))
     for i in range(len(text)):
         while rand_word[i] != text[i]: 
@@ -14,9 +14,22 @@ def TxtPerLetter(text, speed):
             rand_word = (rand_word[0:i]) + (id_generator(1)) + (rand_word[i+1:])
         print(rand_word + "🟢") 
 
+def TxtPerLetter(text, speed):
+    rand_word = (id_generator(len(text)))
+    while rand_word != text:
+    
+
+        for i in range(len(text)):
+            if rand_word[i] == text[i] : print(f"{rand_word[i]} IN {text}!!")
+            rand_word = (id_generator(len(text)))           
+
+        time.sleep(speed)
+        print(rand_word)
+
+
         
 def main():
-    TxtPerLetter("HELLOWORLD", 0.03)
+    TxtPerLetter("HELLOWORLD", 1)
 
 
 
