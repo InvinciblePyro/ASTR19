9a4a2ff0f961a78136acfbe47bdf6f390e5f9883
InvinciblePyro <samrexspivey@gmail.com>
2026-04-14 13:00:15 -0700

    i THINK session 5 is done?

---
+++ Journal1/Session5.py
@@ -0,0 +1,17 @@
+import numpy as np
+from astropy.table import Table
+
+x = np.linspace(0, 2*np.pi, 1000)
+sinx = np.sin(x)
+
+def main():
+    t1 = Table()
+    t1['sin(x)'] = sinx
+    t1['x'] = x
+    t1.pprint_all()
+
+
+
+    
+if __name__ == "__main__": main()
+
