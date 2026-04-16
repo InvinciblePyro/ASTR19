d26c6c2715be607492fe08ed16506b0591e18f0d
InvinciblePyro <samrexspivey@gmail.com>
2026-04-16 12:59:14 -0700

    done with session 6

--- Quizzes/Quiz1.py
+++ Quizzes/Quiz1.py
@@ -1,3 +1,19 @@
-f = open('data_hw3.txt','r')
-f.readlines()
+import datetime
 
+def main():
+
+  #define a date
+
+  s = '2022-03-14T15:09:26Z'
+
+  #format the date
+
+  d = datetime.datetime.strptime(s,"%Y-%m-%dT%H:%M:%SZ")
+
+  #print the date and time
+
+  print(d)
+
+if __name__=="__main__":
+
+  main()
